4b06792d913314754107dcb5af19f8b42b4afb15
Nick Pocock <pocockn@hotmail.co.uk>
2018-08-21 14:52:01 +0100

    Change to delete method that takes a DeleteItemInput as previous method wouldn't work with composite keys. (#40)
    * Change to delete method that takes a DeleteItemInput as previous method wouldn't work with composite keys.
    
    * Bump version.
    
    * Change interface to return a map

--- dynamodb/deletable.go
+++ dynamodb/deletable.go
@@ -3,7 +3,7 @@ package dynamodb
 type (
 	// Deletable represents an object that can be deleted within Dynamo.
 	Deletable interface {
-		Key() string
+		Key() map[string]interface{}
 		TableName() string
 	}
 )
